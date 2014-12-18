bb61878698d68e8190dc102d9e4ae9d77613395d
Thomas Alexander Gerds <tag@biostat.ku.dk>
2014-12-18 12:09:03 +0100

    some progress

--- R/agreement.R
+++ R/agreement.R
@@ -2,25 +2,23 @@
 ##' list of lists.
 ##' 
 ##' @title Measure agreement between a set of lists
-##' @param object Either matrix where each column is a list or a list of lists.
+##' @param object Either matrix where each column is a list or a list of lists. Elements are integers between 1 and the length of the lists.
 ##' @param metric Function used to be applied to the ranks
-##' @param depth Interest margin, i.e., agreement is limited to the first \code{depth} elements of each list.
+##' @param depth Interest margin, i.e., measurement of agreement is limited to the first \code{depth} elements of each list.
 ##' @return Vector of lengths \code{depth} where element e contains the result
 ##' of metric applied to the ranks of the lists at position e.
 ##' @seealso 
 ##' @examples
 ##' agreement(list(c(1,2,3,4,5,7,6,8), c(1,2,3,5,6,4,7,8)))
-##' agreement(list(c(1,2,3,4,5,7,6,8,9,10,11,12,13), c(5,11,4,7,8,3,12,13,6,10,9,2,1)),depth=4)
+##' agreement(list(c(1,2,3,4,5,7,6,8,9,10,11,12,13), c(5,11,4,7,8,3,12,13,6,10,9,2,1)),depth=13,metric=sd)
+##' agreement(list(c(1,2,3,4,5,7,6,8,9,10,11,12,13), c(5,11,4,7,8,3,12,13,6,10,9,2,1)),depth=4,metric=sd)
 ##' 
 ##' ## Simulate some data
 ##' library(lava)
 ##' m <- lvm()
-##' nvar <- 1000
+##' nvar <- 100
 ##' addvar(m) <- c("Y",paste("X",1:nvar,sep=""))
-##' ## x <- matrix(rnorm(nobs*nvar), ncol=nvar)
-##' ## nsig <- 1:5
 ##' regression(m,Y~X1+X2+X3+X4+X5) <- rnorm(5,0.5,0.5)
-##' ## y <- rnorm(nobs, mean = 0*rowSums(x[,nsig]))
 ##' nobs <- 100
 ##' d <- sim(m,nobs)
 ##' 
@@ -42,19 +40,60 @@
 ##' 
 ##' # Forest
 ##' library(randomForestSRC)
-##' f1 <- rfsrc(Y~.,data=d,ntree=10)
+##' f1 <- rfsrc(Y~.,data=d,ntree=1000)
+##' z2 <- agreement(list(order(abs(f1$importance),decreasing=TRUE),l2))
+##' plot(z2,xlim=c(0,20))
+##' 
+##' ## f1 <- rfsrc(Y~X1+X2+X77+X79.,data=d,ntree=10)
 ##' l4 <- order(max.subtree(f1, conservative = TRUE,max.order=1)$order)
 ##' L4 <- max.subtree(f1,maxorder=, conservative = TRUE,max.order=0)$order
-##' agreement(L4)
+##' L4a <- apply(jitter(L4),2,order)
+##' agreement(L4a)
+##' agreement(L4a[,1:4])
+##'
+##' boot.list <- lapply(1:10,function(b){
+##' d.b <- d[sample(1:NROW(d),replace=TRUE),]
+##' xx.b <- scale(d.b[,-1])
+##' y.b <- d.b[,1]
+##' res.b <- coef(glmnet(xx.b,y.b), s=lambda)[-1]
+##' order(abs(res.b), decreasing=TRUE)
+##' })
+##' a.re <- agreement(boot.list)
+##' sub.list <- lapply(1:10,function(b){
+##' d.b <- d[sample(1:NROW(d),replace=FALSE,size=80),]
+##' xx.b <- scale(d.b[,-1])
+##' y.b <- d.b[,1]
+##' res.b <- coef(glmnet(xx.b,y.b), s=lambda)[-1]
+##' order(abs(res.b), decreasing=TRUE)
+##' })
+##' a.sub <- agreement(sub.list)
+##' plot(a.re)
+##' plot(a.sub,add=TRUE,lines.col=2)
+##' boot.forest <- lapply(1:10,function(b){
+##' d.b <- d[sample(1:NROW(d),replace=TRUE),]
+##' f1 <- rfsrc(Y~.,data=d.b,ntree=100,nsplit=10)
+##' order(abs(f1$importance),decreasing=TRUE)
+##' })
+##' forest.re <- agreement(boot.forest)
+##' sub.forest <- lapply(1:10,function(b){
+##' d.b <- d[sample(1:NROW(d),replace=FALSE,size=80),]
+##' f1 <- rfsrc(Y~.,data=d.b,ntree=100,nsplit=10)
+##' order(abs(f1$importance),decreasing=TRUE)
+##' })
+##' forest.sub <- agreement(sub.forest)
+##' plot(forest.re,xlim=c(0,20))
+##' plot(forest.sub,add=TRUE,lines.col=2)
+##'
+##' 
 ##' @export 
 ##' @author Claus Ekstrøm <ekstrom@@sund.ku.dk>, Thomas A. Gerds <tag@@biostat.ku.dk>
 agreement <- function(object, metric=var, depth=NULL) {
     metric <- match.fun(metric)
-    if (missing(depth)) depth <- length(list[[1]])
     if (is.matrix(object))
         rankmat <- object
     else
-        rankmat <- as.matrix(do.call("cbind",lapply(object,function(l)l[1:depth])))
+        rankmat <- as.matrix(do.call("cbind",object))
+    if (missing(depth)) depth <- NROW(rankmat)
     # Compute rank for each item per list
     itemranks <- apply(rankmat, 2, order)
     rankmat.l <- lapply(seq_len(nrow(rankmat)), function(i) rankmat[i,])  
--- R/plot.agreement.R
+++ R/plot.agreement.R
@@ -15,7 +15,7 @@
 #' @method plot agreement
 #' @S3method plot agreement
 ##' @author Thomas A. Gerds <tag@@biostat.ku.dk>
-plot.agreement <- function(x,xlim,ylim,xlab="List depth",ylab="Agreement",...){
+plot.agreement <- function(x,xlim,ylim,xlab="List depth",ylab="Agreement",add=FALSE,...){
     if (missing(xlim)) xlim=c(0,x$depth)
     if (missing(ylim)) ylim=range(x$agreement)
     axis1.DefaultArgs <- list()
@@ -23,19 +23,21 @@ plot.agreement <- function(x,xlim,ylim,xlab="List depth",ylab="Agreement",...){
     plot.DefaultArgs <- list(x=0,y=0,type = "n",ylim = ylim,xlim = xlim,xlab = xlab,ylab = ylab)
     lines.DefaultArgs <- list(type="l",lwd=3,x=1:x$depth,y=x$agreement)
     smartA <- prodlim::SmartControl(call=list(...),
-                          keys=c("plot","lines","axis1","axis2"),
-                          ignore=c("x","ylim","xlim","xlab","ylab"),
-                          defaults=list("plot"=plot.DefaultArgs,
-                              "lines"=lines.DefaultArgs,
-                              "axis1"=axis1.DefaultArgs,
-                              "axis2"=axis2.DefaultArgs),
-                          forced=list("plot"=list(axes=FALSE),"axis1"=list(side=1)),
-                          ignore.case=TRUE,
-                          replaceDefaults=FALSE,
-                          verbose=TRUE)
-    do.call("plot",smartA$plot)
-    ## plot(0,0,type="n",axes=FALSE,xlab="Depth",ylab="")
-    do.call("axis",smartA$axis1)
-    do.call("axis",smartA$axis2)
+                                    keys=c("plot","lines","axis1","axis2"),
+                                    ignore=c("x","ylim","xlim","xlab","ylab"),
+                                    defaults=list("plot"=plot.DefaultArgs,
+                                        "lines"=lines.DefaultArgs,
+                                        "axis1"=axis1.DefaultArgs,
+                                        "axis2"=axis2.DefaultArgs),
+                                    forced=list("plot"=list(axes=FALSE),"axis1"=list(side=1)),
+                                    ignore.case=TRUE,
+                                    replaceDefaults=FALSE,
+                                    verbose=TRUE)
+    if (add==FALSE){
+        do.call("plot",smartA$plot)
+        ## plot(0,0,type="n",axes=FALSE,xlab="Depth",ylab="")
+        do.call("axis",smartA$axis1)
+        do.call("axis",smartA$axis2)
+    }
     do.call("lines",smartA$lines)
 }
