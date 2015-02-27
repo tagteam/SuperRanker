ca83f7c515674ef4f1c499409cd7bbea0e45ee70
Thomas Alexander Gerds <tag@biostat.ku.dk>
2015-02-27 11:25:19 +0100

    arbejds session towards state-of-the art

--- R/plot.agreement.R
+++ R/plot.agreement.R
@@ -9,13 +9,27 @@
 ##' @param ... Processed by \code{\link{prodlim::SmartControl}}.
 ##' @return Graph
 ##' @examples
-##' a <- agreement(list(c(1,2,3,4,5,7,6,8,9,10,11,12,13), c(5,11,4,7,8,3,12,13,6,10,9,2,1)))
+##' R1=c(1,2,3,4,5,7,6,8,9,10,11,12,13)
+##' R2=c(5,11,4,7,8,3,12,13,6,10,9,2,1)
+##' a <- agreement(list(R1,R2))
 ##' plot(a)
+##' arand = colMeans(do.call("rbind",lapply(1:20,function(b){
+##'     agreement(list(sample(R1),sample(R1)))$agreement
+##' })))
+##' lines(1:length(R1),arand,col=2,lwd=3)
+##' 
+##' l <- c(1,2,3,4,5,7,6,8,9,10,11,12,13)
+##' l <- 0:100
+##' aa <- sapply(1:20,function(i){
+##'     agreement(list(sample(l),sample(l),sample(l)))$agreement[1]
+##' })
+##' c(mean(aa),sd(aa))
+##' 
 #' @export
 ##' @author Thomas A. Gerds <tag@@biostat.ku.dk>
 plot.agreement <- function(x,xlim,ylim,xlab="List depth",ylab="Agreement",add=FALSE,...){
-    if (missing(xlim)) xlim=c(0,x$depth)
-    if (missing(ylim)) ylim=range(x$agreement)
+    if (missing(xlim)) xlim=c(1,x$depth)
+    if (missing(ylim)) ylim=c(0,max(x$agreement))
     axis1.DefaultArgs <- list()
     axis2.DefaultArgs <- list(side=2)
     plot.DefaultArgs <- list(x=0,y=0,type = "n",ylim = ylim,xlim = xlim,xlab = xlab,ylab = ylab)
