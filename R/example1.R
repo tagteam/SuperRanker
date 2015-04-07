45ee409e9f76743563ded8bf84eb6fe365583d7f
ekstroem <github@ekstroem.com>
2015-04-07 14:51:17 +0200

    asd

--- R/example1.R
+++ R/example1.R
@@ -3,85 +3,116 @@
 # Read data
 #
 #
-
+library(glmnet)
+library(minerva)
+library(changepoint)
+library(randomForestSRC)
 
 source("sra.R")
 
-
 # Read data
-snps <- read.table("http://people.umass.edu/foulkes/asg/data/snpSimCVDNum.txt", header=TRUE)
-traits <- read.table("http://people.umass.edu/foulkes/asg/data/ySimCVD.txt", header=TRUE)
+library(multtest)
+data(golub)
 
 
-y <- traits$yA  
-x <- snps
-index <- seq(1, NCOL(snps))
-d <- data.frame(y, x)
-#md <- mice(d)
-
-# Marginal t-tests
-
-mt.p <- sapply(index, function(i) { anova(lm(y ~ x[,i]))[1,5] } )
-list1 <- order(mt.p)
-
-# Elastic net
-
-library(glmnet)
-#library(mice)
-#res <- coef(glmnet(as.matrix(x), y, alpha=0), s=lambda)[-1]
-
-# MIC
-
-library(minerva)
-MIC <- sapply(index, function(i) { cc <- complete.cases(snps[,i],y) ; mine(x[cc,i], y[cc])$MIC})
-list3 <- order(MIC, decreasing=TRUE)
-
-## Forest
-library(randomForestSRC)
-f1 <- rfsrc(y~., data=d, ntree=1000)
-variables <- abs(f1$importance)
-num.undecided <- sum(variables==0)
-list4 <- order(variables, decreasing=TRUE)
-list4[(length(variables)-num.undecided):length(variables)] <- 0
+y <- golub.cl
+x <- golub 
 
 
-res <- sra(list(list1, list3, list4))
-plot(sqrt(res$agreement),xlim=c(0,20))
+vs.vimp.md <- function(obj, which.outcome, conservative = TRUE) {
+   msub <- max.subtree(obj, conservative = conservative)
+   vimp <- obj$importance
+   if (is.null(vimp)) {
+       vimp <- matrix(1, nrow(msub$order), ncol(obj$xvar))
+   }
+   names <- rownames(msub$order)
+   names[vimp[, which.outcome] > 0 & msub$order[, 1] <= msub$threshold]    
+} 
+
+producelists <- function(x, y) {
+    index <- seq(1, nrow(golub))
+                                        # d <- data.frame(y, x)
+
+                                        # Marginale t-tests
+    mt.p <- sapply(index, function(i) { t.test(x[i,] ~ y)$p.value } )
+    list1 <- order(mt.p)
+    
+                                        # Marginale logreg-tests
+    mlogreg.p <- sapply(index, function(i) { drop1(glm(y ~ x[i,], family=binomial), test="Chisq")[2,5] } )
+    list2 <- order(mlogreg.p)
+    
+                                        # Elastic net
+    X <- scale(t(x))
+    enet <- glmnet(X, y, family="binomial", alpha=.8)
+    nyres <- cv.glmnet(X, y, family="binomial", alpha=.8)
+    coefficients <- coef(enet, s=nyres$lambda.1se)[-1]
+    nonzeros <- sum(coefficients!=0)
+    list3 <- order(abs(coefficients), decreasing=TRUE)
+    
+                                        # MIC
+    MIC <- sapply(index, function(i) { mine(x[i,], y)$MIC})
+    list4 <- order(MIC, decreasing=TRUE)
+    
+    
+                                        # Random Forest
+    dd <- data.frame(y=factor(y), t(x))
+                                        # dd <- data.frame(y=y, t(x))
+    f1 <- rfsrc(y ~ ., data=dd, ntree=100)
+    variables <- abs(f1$importance[,1])
+    num.undecided <- sum(variables==0)
+    list5 <- order(variables, decreasing=TRUE)
+    list5[(length(variables)-num.undecided):length(variables)] <- 0
+    
+    cbind(list1,list2,list3,list4,list5)
+}
+
+inputmatrix <- producelists(x, y)
 
 
+res <- sra(inputmatrix)
+
+plot(sqrt(res$agreement)[1:200], lwd=3, col="red", type="l")
 
 
 ####
 ####
 ####
 
+makeTransparent<-function(someColor, alpha=100)
+{
+  newColor<-col2rgb(someColor)
+  apply(newColor, 2, function(curcoldata){rgb(red=curcoldata[1], green=curcoldata[2],
+    blue=curcoldata[3],alpha=alpha, maxColorValue=255)})
+}
 
-nulleval <- sapply(1:2, function(i) {
+random.list <- function(x, nsim=100) {
+    npreds <- nrow(x)
+
+    res <- sapply(1:nsim, function(i) {
+        
+        xx <- sapply(1:ncol(x), function(ii) { sample(1:npreds) } )
+        sra(xx, B=1, listlength=nrow(x))$agreement
+    } )
+    res
+}
+
+newcol <- makeTransparent
+
+null1 <- random.list(inputmatrix)
+
+matlines(null1, type="l", col="lightgray")
+
+
+nsims <- 20
+
+nulleval <- sapply(1:nsims, function(i) {
     py <- sample(y)
 
-    #1
-    mt.p <- sapply(index, function(i) { anova(lm(py ~ x[,i]))[1,5] } )
-    list1 <- order(mt.p)
-
-    #3
-    MIC <- sapply(index, function(i) { cc <- complete.cases(snps[,i],py) ; mine(x[cc,i], py[cc])$MIC})
-    list3 <- order(MIC, decreasing=TRUE)
-
-    #4
-    dd <- d
-    dd$y <-py
-    f1 <- rfsrc(y~., data=dd, ntree=1000)
-    variables <- abs(f1$importance)
-    num.undecided <- sum(variables==0)
-    list4 <- order(variables, decreasing=TRUE)
-    list4[(length(variables)-num.undecided):length(variables)] <- 0
-
-    res <- sra(list(list1, list3, list4))$agreement
+    inputmatrix <- producelists(x, py)
+    
+    res <- sra(inputmatrix)$agreement
     res
 })
 
-nulleval1 <- sapply(1:2, function(i) {
-    nobs <- length(list1)
-    res <- sra(list(rep(0, nobs), rep(0, nobs), rep(0, nobs)), listlength=nobs)$agreement
-})
 
+    
